d4824e6d9c38fcd67a484a7b44c14d3303d285a9
Arihant Sharma <126870848+ArihantSharma@users.noreply.github.com>
2026-01-10 01:43:51 +0530

    Mining added

--- Models/blockchain.py
+++ Models/blockchain.py
@@ -2,42 +2,49 @@ from Models.block import Block, create_genesis_block
 import time
 
 class Blockchain:
-    def __init__(self):
+    def __init__(self, difficulty=4):
+        self.difficulty = difficulty
         self.chain = [create_genesis_block()]
+
 
     def get_latest_block(self):
         return self.chain[-1]
 
     def add_block(self, data):
-        latest_block = self.get_latest_block()
+        latest_block = self.chain[-1]
         new_block = Block(
             index=latest_block.index + 1,
             timestamp=time.time(),
             data=data,
             prev_hash=latest_block.hash
         )
+
+        new_block.mine_block(self.difficulty)
         self.chain.append(new_block)
 
     def is_chain_valid(self):
         for i in range(1, len(self.chain)):
-            current = self.chain[i]
-            previous = self.chain[i - 1]
+            curr = self.chain[i]
+            prev = self.chain[i - 1]
 
-            if current.hash != current.calculate_hash():
+            if curr.hash != curr.calculate_hash():
                 return False
 
-            if current.prev_hash != previous.hash:
+            if curr.prev_hash != prev.hash:
+                return False
+
+            if curr.hash[:self.difficulty] != "0" * self.difficulty:
                 return False
 
         return True
 
-if __name__ == "__main__":
-    bc = Blockchain()
+# if __name__ == "__main__":
+#     bc = Blockchain()
 
-    bc.add_block("Credential Hash: ABC123")
-    bc.add_block("Credential Hash: XYZ789")
+#     bc.add_block("Credential Hash: ABC123")
+#     bc.add_block("Credential Hash: XYZ789")
 
-    for block in bc.chain:
-        print(vars(block))
+#     for block in bc.chain:
+#         print(vars(block))
 
-    print("Blockchain valid?", bc.is_chain_valid())
+#     print("Blockchain valid?", bc.is_chain_valid())
